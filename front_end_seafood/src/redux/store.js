514992944f3f34cea8d612802f290de38759b5ae
CheThanh <chethanh3112@gmail.com>
2023-12-11 23:04:21 +0700

    feat: redux persist

--- front_end_seafood/src/redux/store.js
+++ front_end_seafood/src/redux/store.js
@@ -1,9 +1,33 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import authReducer from "./authSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export const store = configureStore({
-    reducer:{
-        auth: authReducer
-    }
-})
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  const rootReducer = combineReducers({auth: authReducer})
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  })
+  export let persistor = persistStore(store);
 export default store;
