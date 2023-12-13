21d9e5e3ab961efbc9dc5eb1df7ae78f96ae214e
CheThanh <chethanh3112@gmail.com>
2023-12-13 23:48:30 +0700

    feat: cartSlice

--- front_end_seafood/src/redux/cartSlice.js
+++ front_end_seafood/src/redux/cartSlice.js
@@ -1,10 +1,18 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice, current } from "@reduxjs/toolkit"
 
 const cartSlice = createSlice({
     name:"cart",
     initialState:{
         addToCart:{
-            data:null,
+            isFetching:false,
+            error:false
+        },
+        getCart:{
+            data:[],
+            isFetching:false,
+            error:false
+        },
+        deleteItem:{
             isFetching:false,
             error:false
         }
@@ -13,21 +21,71 @@ const cartSlice = createSlice({
         addToCartStart: (state)=>{
             state.addToCart.isFetching = true;
         },
-        addToCartSuccess: (state,action) =>{
+        addToCartSuccess: (state) =>{
             state.addToCart.isFetching = false;
-            state.addToCart.data= action.payload;
             state.addToCart.error=false;
         },
         addToCartFailed: (state) =>{
-        state.addToCart.isFetching= false;
-        state.addToCart.error =true;
+            state.addToCart.isFetching= false;
+            state.addToCart.error =true;
+        },
+        getCartStart: (state) => {
+            state.getCart = {
+                ...state.getCart,
+                isFetching: true
+            };
+        },
+        getCartSuccess: (state, action) => {
+            state.getCart = {
+                ...state.getCart,
+                isFetching: false,
+                data: action.payload,
+                error: false
+            };
+        },
+        getCartFailed: (state) => {
+            state.getCart = {
+                ...state.getCart,
+                isFetching: false,
+                error: true
+            };
+        },
+        deleteItemStart: (state) => {
+            state.deleteItem = {
+                ...state.deleteItem,
+                isFetching: true
+            };
+        },
+        deleteItemSuccess: (state, action) => {
+            state.deleteItem = {
+                ...state.deleteItem,
+                isFetching: false,
+                error: false
+            };
+            state.getCart.data = state.getCart.data.filter(
+                (item) => item.id !== action.payload
+              );
+        },
+        deleteItemFailed: (state) => {
+            state.deleteItem = {
+                ...state.deleteItem,
+                isFetching: false,
+                error: true
+            };
         }
+
     }
 })
 export const {
     addToCartStart,
   addToCartSuccess,
-  addToCartFailed
+  addToCartFailed,
+  getCartStart,
+  getCartSuccess,
+  getCartFailed,
+  deleteItemStart,
+  deleteItemSuccess,
+  deleteItemFailed
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
