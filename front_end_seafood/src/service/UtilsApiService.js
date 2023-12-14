0a4ebb156603df6b20f2e026838665ba0fe02f3e
CheThanh <chethanh3112@gmail.com>
2023-12-15 06:57:56 +0700

    refactor: utils apis

--- front_end_seafood/src/service/UtilsApiService.js
+++ front_end_seafood/src/service/UtilsApiService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const EMLOYEE_API_BASE_URL="http://localhost:8080/api";
+const UTILS_API_BASE_URL="http://localhost:8080/api";
 // const CREATE_EMPLOYEE="http://localhost:8080/api/createemployees";
 class UtilsApiService{
     UploadImage(formData){
-        return axios.post(EMLOYEE_API_BASE_URL+'/upload',formData);
+        return axios.post(UTILS_API_BASE_URL+'/upload',formData);
     }
     UploadImageImgur(formData){
-        return axios.post(EMLOYEE_API_BASE_URL+'/imgur/upload',formData);
+        return axios.post(UTILS_API_BASE_URL+'/imgur/upload',formData);
     }
     
 }
